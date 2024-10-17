76d38f1b293f3574dd95859146bfbeb0b10f7c28
PhilipeeX <dev.philipe@outlook.com>
2024-10-17 19:37:08 -0300

    adds keymaps for navigation between buffers

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,9 @@
 -- Mapeamento para desidentação com Shift-Tab
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
+
+-- Mapeia Alt + h para ir para o buffer à esquerda
+vim.api.nvim_set_keymap('n', '<A-h>', ':bprevious<CR>', { noremap = true, silent = true })
+
+-- Mapeia Alt + l para ir para o buffer à direita
+vim.api.nvim_set_keymap('n', '<A-l>', ':bnext<CR>', { noremap = true, silent = true })
+
